cd3f794e48a0e9100fe7766bb08787b36fefc4f3
G4brielTerto <gabrieltertoprezoto@gmail.com>
2023-10-06 17:49:40 -0300

    atualização da tabela de pareto
    atualização 1.3

--- login.py
+++ login.py
@@ -4,13 +4,13 @@ import os
 import statistics
 import numpy
 
+
+
 banco=sqlite3.connect('banco_sistema.db')
 cursor=banco.cursor()
 
-#cursor.execute("CREATE TABLE usuario (nome text, idade integer, email tex)")
-#banco.commit()
 
-#cursor.execute("CREATE TABLE usuarios (nome text, idade int, email text, senha)")
+# cursor.execute("CREATE TABLE usuarios (nome text, idade int, email text, senha)")
 
 
 
@@ -46,73 +46,83 @@ def login():
 
 def sistema_pareto():
     a=''
-    cont="s"
-    lista=[]
+    cont="sim"
     titulo="TABELA DE PARETO"
     
+    print("")
     print(f'{a:-^80}')
     print(f'{titulo:-^80}')
     print(f'{a:-^80}')
-
-    # dados_linhas=[]
-    ocorrencias=[]
-
-    while cont=="s":
-        dado=[]
-        dado.append(input("Digite um dado: \n"))
-        # dados_linhas.append(dado)
-        ocor=[]
-        ocor.append(int(input("Digite o número de ocorrências: \n")))
-        teste=ocor
-        ocorrencias.append(teste)
-        # COMO FAZER COM QUE A VARIAVEL OCORRENCIAS RECEBA OS VALORES INTEIRAMENTE DENTRO DO ARRAY
-        # EX: NÃO=> [[1][2][3]] e SIM=>[1,2,3]
-        lista.append(dado+ocor)
-        # print(ocorrencias)
-        cont=input('deseja continuar?\n')
-        # print(ocorrencias)
-
+    print("")
 
     
+    dado=[]
+    ocorrencias=[]
+    ocor=[]
 
-    print(ocorrencias)
-
-    print(f'{a:-^80}')
-    for i in lista:
-            print(f"|{i[0]:^35}| {i[1]:^35}|")
-    print(f'{a:-^80}')
-
-    maior=max(ocorrencias)
-    menor=min(ocorrencias)
-    ampli=maior-menor
-    media=statistics.mean(ocorrencias)
-    mediana = statistics.median(ocorrencias)
-    moda = statistics.mode(ocorrencias)
-    quartil1=numpy.quantile(ocorrencias, 0.25)
-    quartil2=numpy.quantile(ocorrencias, 0.5)
-    quartil3=numpy.quantile(ocorrencias, 0.75)
-    iqr=quartil3-quartil1
+    while cont=="sim":
+        dado.append(input("Digite uma ocorrência: \n"))
+        var=input("Digite o número de ocorrências: \n")
+        ocor.append(var)
+        ocorrencias.append(int(var))
+        # print(ocorrencias)
+        cont=input('deseja continuar?(Digite [sim] ou [nao])\n')
+        if cont != "sim" and cont != "nao":
+            print("Erro!!! Digite as respostas corretas!")
+            print("")
+            cont="sim"
+        # print(ocorrencias)
+    if cont=="nao":
+        print("")
+        print(f'{a:-^80}')
+        print(f'{titulo:^80}')
+        print(f'{a:-^80}')
+        for i in range(0,len(dado)):
+                print(f"|{dado[i] [0]:^35}| {ocor[i] [0]:^35}|")
+        print(f'{a:-^80}')
+        print("")
+        print("")
         
-    print("\n")
-    print("MAIOR valor: ",maior)
-    print("MENOR valor: ",menor)
-    print("AMPLITUDE: ",ampli)
-    print("A MÉDIA é: ",media)
-    print("A MEDIANA é: ",mediana)
-    print("A MODA é: ",moda)
-    print("o 1ºQUARTIL é: ",quartil1)
-    print("o 2ºQUARTIL é: ",quartil2)
-    print("o 3ºQUARTIL é: ",quartil3)
-    print("o IQR é: ",iqr)
-    print()
-    print("O que deseja realizar?")
-    print("Digite [1] para voltar a tela inicial.")
-    print("Digite [0] para sair.\n")
-    c=int(input(":"))
-    if c==0:
-        print("VOCÊ SAIU DO SISTEMA!!!")
-    elif c==1:
-        tela_inicio()
+
+
+        maior=max(ocorrencias)
+        menor=min(ocorrencias)
+        ampli=maior-menor
+        media=statistics.mean(ocorrencias)
+        mediana = statistics.median(ocorrencias)
+        moda = statistics.mode(ocorrencias)
+        quartil1=numpy.quantile(ocorrencias, 0.25)
+        quartil2=numpy.quantile(ocorrencias, 0.5)
+        quartil3=numpy.quantile(ocorrencias, 0.75)
+        iqr=quartil3-quartil1
+        variancia=numpy.var(ocorrencias)
+        dp=numpy.std(ocorrencias)
+            
+        print("\n")
+        print("MAIOR valor: ",maior)
+        print("MENOR valor: ",menor)
+        print("AMPLITUDE: ",ampli)
+        print("A MÉDIA é: ",media)
+        print("A MEDIANA é: ",mediana)
+        print("A MODA é: ",moda)
+        print("o 1ºQUARTIL é: ",quartil1)
+        print("o 2ºQUARTIL é: ",quartil2)
+        print("o 3ºQUARTIL é: ",quartil3)
+        print("o IQR é: ",iqr)
+        print("O total de variância é de: ",variancia)
+        print("O total do desvio padrão é de: ",dp)
+        print("")
+
+
+        print("O que deseja realizar?")
+        print("Digite [1] para voltar a tela inicial.")
+        print("Digite [0] para sair.\n")
+        c=int(input(":"))
+        if c==0:
+            print("VOCÊ SAIU DO SISTEMA!!!")
+            exit()
+        elif c==1:
+            tela_inicio()
     
 
 def tela_inicio():
@@ -126,7 +136,7 @@ def tela_inicio():
         print("Digite [0] para sair.\n")
         c=int(input(":"))
         if c==0:
-            break
+            exit()
         elif c==1:
             cadastro()
         elif c==2:
